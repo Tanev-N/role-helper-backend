e9bb4b2efade3e5d5aa658f287fc94bd3359aa73
Софья <[sonya_pr@bk.ru]>
2025-11-08 19:29:56 +0300

    auth

--- internal/delivery/middleware/auth.go
+++ internal/delivery/middleware/auth.go
@@ -16,6 +16,7 @@ func Auth(us models.UserService) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			cookie, err := r.Cookie("auth_token")
 			if err == nil {
+				log.Println(cookie.Value)
 				token := cookie.Value
 				user, err := us.ValidateToken(token)
 				if err == nil && user != nil {
