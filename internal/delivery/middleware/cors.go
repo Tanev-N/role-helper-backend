3a0b00664ffb2f6ebf382b6a31b95dcce5db9bce
Belyak Sofya <sonyabely@gmail.com>
2025-11-13 00:06:24 +0300

    change postgres

--- internal/delivery/middleware/cors.go
+++ internal/delivery/middleware/cors.go
@@ -3,16 +3,9 @@ package middleware
 import "net/http"
 
 func CORS(next http.Handler) http.Handler {
-	allowedOrigins := map[string]struct{}{
-		"https://critical-roll.ru": {},
-		"http://localhost:8081":    {},
-	}
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-		if _, ok := allowedOrigins[origin]; ok {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-		}
+		w.Header().Set("Access-Control-Allow-Origin", "https://critical-roll.ru")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8081")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Range")
 		w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Content-Range, Accept-Ranges")
