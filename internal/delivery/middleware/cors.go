5deeb5a43a8befd07fa8e74f32faceeaff96e33a
Belyak Sofya <sonyabely@gmail.com>
2025-11-12 18:41:03 +0300

    change character

--- internal/delivery/middleware/cors.go
+++ internal/delivery/middleware/cors.go
@@ -3,8 +3,16 @@ package middleware
 import "net/http"
 
 func CORS(next http.Handler) http.Handler {
+	allowedOrigins := map[string]struct{}{
+		"https://critical-roll.ru": {},
+		"http://localhost:8081":    {},
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://critical-roll.ru")
+		origin := r.Header.Get("Origin")
+		if _, ok := allowedOrigins[origin]; ok {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Range")
 		w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Content-Range, Accept-Ranges")
